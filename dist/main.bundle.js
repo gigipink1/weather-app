0d050b9ff5e7aaa7b7b9b1b5716298a8cd6a0c43
Sascha H <146263983+gigipink1@users.noreply.github.com>
2024-06-19 02:02:39 +0100

    Initial commit

---
+++ dist/main.bundle.js
@@ -0,0 +1,18 @@
+(self.webpackChunkrepo_template = self.webpackChunkrepo_template || []).push([["main"],{
+
+/***/ "./src/index.js":
+/*! **********************!*\
+  !*** ./src/index.js ***!
+  \********************* */
+/***/ (() => {
+
+
+
+/***/ })
+
+},
+/** *** */ __webpack_require__ => { // webpackRuntimeModules
+/** *** */ const __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
+/** *** */ const __webpack_exports__ = (__webpack_exec__("./src/index.js"));
+/** *** */ }
+]);
---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    static: './src',
+  },
+  plugins: [    new HtmlWebpackPlugin({
+    template: './src/index.html',
+    inject: 'body',
+  }),
+  ],
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+   clean: true,
+  },
+  optimization: {
+    runtimeChunk: 'single',
+  },
+};
